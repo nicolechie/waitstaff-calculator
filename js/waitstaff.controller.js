293f4207f3f76e501cc821e65fd44f07eda4ed04
nicolechie <nicolechie@mac.com>
2016-03-31 18:57:07 -0600

    routes added

--- js/waitstaff.controller.js
+++ js/waitstaff.controller.js
@@ -1,58 +1,90 @@
-angular.module('waitstaff',[
-    ])
-.controller('WaitstaffController', ['$scope', function($scope){
-// This will be the controller variables
-var vm = this;
-	vm.count = 0;
-	vm.subtotal = 0;
-	vm.tip = 0;
-
-	var tipTotals = [];
-	vm.tipTotals = tipTotals;
-
-// This will be the controller methods
-vm.submitOrder = submitOrder;
-vm.cancel = cancel;
-vm.reset = reset;
-vm.calculateTipTotal = calculateTipTotal;
-
-activate();
-
-	function activate() {
-		vm.taxRate=5;
-	}
-
-	function submitOrder() {
-		vm.count += 1;
-		vm.tax = vm.price * (vm.taxRate/100);
-		vm.subtotal = vm.price + vm.tax;
-		vm.tip = vm.price * (vm.tipPercentage/100);
-		vm.tipTotals.push(vm.tip);
-	}
-
-	 function calculateTipTotal() {
-		var tipTotal = 0;
-		for (var i=0; i<vm.tipTotals.length; i++) {
-			tipTotal+=tipTotals[i];
-		}
-		return tipTotal;
-	}
-
-	function cancel () {
-		vm.price = '';
-		vm.tipPercentage = '';
-		vm.taxRate = '';
-	}
-
-	function reset() {
-		vm.price = '';
-		vm.tipPercentage = '';
-		vm.taxRate = '';
+angular.module('waitstaff',['ngRoute'])
+	.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider){
+    	$locationProvider.html5Mode(true);
+        $routeProvider
+        .when('/', {
+            templateUrl : 'home.html',
+            controller : 'WaitstaffController'
+        })
+        .when('/new-meal', {
+		    templateUrl : 'new-meal.html',
+		    controller : 'WaitstaffController'
+		})
+		.when('/my-earnings', {
+		    templateUrl : 'my-earnings.html',
+		    controller : 'WaitstaffController'
+		})
+		.when('/error', {
+		    template : '<p>Error - Page Not Found</p>'
+		})
+		// .run(function($rootScope, $location) {
+		//     $rootScope.$on('$routeChangeError', function() {
+		//         $location.path('/error');
+		//     });
+		// })
+    }])
+	// .controller('HomeCtrl', ['$scope', function($scope) {
+	//         //empty for now
+	// }])
+	// .controller('NewMealCtrl', ['$scope', function($scope) {
+	//         //empty for now
+	// }])
+	// .controller('MyEarningsCtrl', ['$scope', function($scope) {
+	//         //empty for now
+	// }])
+	.controller('WaitstaffController', ['$scope', function($scope){
+	// This will be the controller variables
+	var vm = this;
 		vm.count = 0;
 		vm.subtotal = 0;
 		vm.tip = 0;
-		vm.tax = 0;
+
 		var tipTotals = [];
 		vm.tipTotals = tipTotals;
-	}
-}]);
+
+	// This will be the controller methods
+	vm.submitOrder = submitOrder;
+	vm.cancel = cancel;
+	vm.reset = reset;
+	vm.calculateTipTotal = calculateTipTotal;
+
+	activate();
+
+		function activate() {
+			vm.taxRate=5;
+		}
+
+		function submitOrder() {
+			vm.count += 1;
+			vm.tax = vm.price * (vm.taxRate/100);
+			vm.subtotal = vm.price + vm.tax;
+			vm.tip = vm.price * (vm.tipPercentage/100);
+			vm.tipTotals.push(vm.tip);
+		}
+
+		 function calculateTipTotal() {
+			var tipTotal = 0;
+			for (var i=0; i<vm.tipTotals.length; i++) {
+				tipTotal+=tipTotals[i];
+			}
+			return tipTotal;
+		}
+
+		function cancel () {
+			vm.price = '';
+			vm.tipPercentage = '';
+			vm.taxRate = '';
+		}
+
+		function reset() {
+			vm.price = '';
+			vm.tipPercentage = '';
+			vm.taxRate = '';
+			vm.count = 0;
+			vm.subtotal = 0;
+			vm.tip = 0;
+			vm.tax = 0;
+			var tipTotals = [];
+			vm.tipTotals = tipTotals;
+		}
+	}]);
